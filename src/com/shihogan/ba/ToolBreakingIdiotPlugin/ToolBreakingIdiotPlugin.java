755d3dce5a3e70185ad799bc257dbe52bf85d94c
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-07-19 12:59:51 -0500

    enabled and disabled overrides

--- src/com/shihogan/ba/ToolBreakingIdiotPlugin/ToolBreakingIdiotPlugin.java
+++ src/com/shihogan/ba/ToolBreakingIdiotPlugin/ToolBreakingIdiotPlugin.java
@@ -3,5 +3,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ToolBreakingIdiotPlugin extends JavaPlugin
 {
+	// Ran when plugin enabled
+	@Override
+	public void onEnable()
+	{
+	}
 
+	// Ran when plugin disabled
+	@Override
+	public void onDisable()
+	{
+	}
 }
